6e0a309112ea5d35562c66dcd406ab5d3f0274e0
paolopez728 <pabloarturolopezp@gmail.com>
2023-10-06 23:57:30 -0600

    PDF

--- client/src/vistas/Facturacion.jsx
+++ client/src/vistas/Facturacion.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useState, useRef } from 'react';
 import { useCarrito } from "../hoocks/carritoState";
-
+import { useReactToPrint } from 'react-to-print';
 
 import '../css/Facturacion.css'
 
 const Facturacion = () => {
 
-    
+    const conponentPDF = useRef();
     const fechaActual = new Date();
     const numeroAleatorio = Math.floor(Math.random() * 1000000) + 1;
     const año = fechaActual.getFullYear();
@@ -14,7 +14,7 @@ const Facturacion = () => {
     const dia = fechaActual.getDate();
     const fechaFormateada = `${año}-${mes < 10 ? '0' : ''}${mes}-${dia < 10 ? '0' : ''}${dia}`;
 
-   
+
     const IVA_RATE = 0.12; // Tasa de IVA del 12%
     const { carrito } = useCarrito();
 
@@ -35,61 +35,73 @@ const Facturacion = () => {
         return totalSubtotal * IVA_RATE;
     };
 
+    const generatePDF = useReactToPrint({
+        content: () => conponentPDF.current,
+        documentTitle: 'Userdata',
+        onAfterPrint: () => alert('PDF generado con éxito')
+    });
+
     return (
         <div className="inicio" id="factura">
-            <div className="invoice-box container_register">
-                <table cellpadding="0" cellspacing="0">
-                    <tr className="top">
-                        <td colspan="4">
-                            <table>
-                                <tr>
-                                    <td class="title">
-                                        <img src="../img/1696568622883.png" alt=""></img>
-                                    </td>
-                                    <td>Factura #:{numeroAleatorio} <br></br>Creada:{fechaFormateada}</td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                    <tr class="top">
-                        <td colspan="4">
-                            <table>
-                                <tr>
-                                    <td>
-                                        Sparksuite, Inc.<br></br>12345 Sunny Road<br></br>Sunnyville, CA 12345
-                                    </td>
-                                    <td>
-                                        Acme Corp.<br></br>John Doe<br></br> john@example.com
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                    <tr class="heading">
-                        <td>Articulo</td>
-                        <td>Costo Unitario</td>
-                        <td>Cantidad</td>
-                        <td>Precio Total</td>
-                    </tr>
-                    {carrito.map((producto, index) => (
-                        <tr class="item" key={producto.ProductoID}>
-                            <td><h5 className=" ">{producto.NombreProducto}</h5></td>
-                            <td><h5>Q.{producto.PrecioVenta.toFixed(2)}</h5></td>
-                            <td><h5>1</h5></td>
-                            <td>Q.{calcularSubtotal(producto).toFixed(2)}</td>
+                <div ref={conponentPDF}className="invoice-box container_register">
+                    <table cellpadding="0" cellspacing="0">
+                        <tr className="top">
+                            <td colspan="4">
+                                <table>
+                                    <tr>
+                                        <td class="title">
+                                            <img src="../img/1696568622883.png" alt=""></img>
+                                        </td>
+                                        <td>Factura #:{numeroAleatorio} <br></br>Creada:{fechaFormateada}</td>
+                                    </tr>
+                                </table>
+                            </td>
                         </tr>
-                    ))}
-                    <tr class="total">
-                        <td colspan="3"></td>
-                        <td>IVA: Q.{calcularIVA().toFixed(2)}</td>
-                    </tr>
-                    <tr class="total">
-                        <td colspan="3"></td>
-                        <td>Total: Q.{(calcularTotalSubtotal() + calcularIVA()).toFixed(2)}</td>
-                    </tr>
-                </table>
+                        <tr class="top">
+                            <td colspan="4">
+                                <table>
+                                    <tr>
+                                        <td>
+                                            Sparksuite, Inc.<br></br>12345 Sunny Road<br></br>Sunnyville, CA 12345
+                                        </td>
+                                        <td>
+                                            Acme Corp.<br></br>John Doe<br></br> john@example.com
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr class="heading">
+                            <td>Articulo</td>
+                            <td>Costo Unitario</td>
+                            <td>Cantidad</td>
+                            <td>Precio Total</td>
+                        </tr>
+                        {carrito.map((producto, index) => (
+                            <tr class="item" key={producto.ProductoID}>
+                                <td><h5 className=" ">{producto.NombreProducto}</h5></td>
+                                <td><h5>Q.{producto.PrecioVenta.toFixed(2)}</h5></td>
+                                <td><h5>1</h5></td>
+                                <td>Q.{calcularSubtotal(producto).toFixed(2)}</td>
+                            </tr>
+                        ))}
+                        <tr class="total">
+                            <td colspan="3"></td>
+                            <td>IVA: Q.{calcularIVA().toFixed(2)}</td>
+                        </tr>
+                        <tr class="total">
+                            <td colspan="3"></td>
+                            <td>Total: Q.{(calcularTotalSubtotal() + calcularIVA()).toFixed(2)}</td>
+                        </tr>
+                    </table>
+
             </div>
+            <div className='d-grid d-md-flex justify-content-md-end mb-3'>
+                <button className='btn btn-success' onClick={generatePDF}>PDF</button>
+            </div>
+
         </div>
+
     );
 };
 
